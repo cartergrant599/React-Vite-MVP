2a1536183959f616bcc118651985a841871699d8
cartergrant599 <cartergrant135@gmail.com>
2024-11-16 08:25:46 +0800

    added gitignore file

--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -13,7 +13,7 @@ const Button: React.FC<ButtonProps> = ({ type = "button", onClick, disabled, chi
       type={type}
       onClick={onClick}
       disabled={disabled}
-      className="inline-block rounded-lg bg-primary px-5 py-2.5 text-sm font-medium text-white focus:ring-0 focus:border-none"
+      className="inline-block rounded-lg bg-primary px-5 py-2.5 text-sm font-medium text-white"
     >
       {children}
     </button>
